83f067943e5759395ba84f19b4580ae6e4aa0caa
gunamkwon <rnska06@mju.ac.kr>
2021-05-12 12:54:57 +0900

    Pi4 thread

--- ESP32/thread.py
+++ ESP32/thread.py
@@ -1,24 +1,117 @@
 import threading
+import time
+import bt2
+from bluetooth import *
 
-from queue import Queue
+led_auto = 0
 
+class LED(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        
+    def run(self):
+        #led = LED.setup()
+        while True:
+            if(led_auto == 1):
+                auto_time = led.time_check() 
+                if(auto_time==1):
+                    led.led_on()
+                else:
+                    led.led_off()
+                time.sleep(0.1)
+            else:
+                print("led_task is manul now")
+                time.sleep(10)
+            
+class LEVEL(threading.Thread):
+    def __init__(self):
+        super().__init__()
+    def run(self):
+        #level = water_level.setup()
+        while True:
+            #ret = water_level.check(level)
+            print("chekcing level\n")
+            ret = 0
+            if(ret != 1):
+                #Bluetooth.send("W")
+                print("sending Message")
+            time.sleep(10)
 
-def creator(data, q):
-    print('Creating Data and putting it on the queue')
-    print('\n')
-    for item in data:
-        evt = threading.Event()
-        q.put((item, evt))
-        print('Waiting for data to be doubled')
-        evt.wait()
+            
+class Motor(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self, name='Motor Thread')
+    
+    def run(self):
+        global tds_Value
+        tds_Value = tds()
+        while 1:
+            #if tds_Value < tds_Min
+            if tds_Value < 100: # when tds value is lower than minimum tds.
+                motor_7run(1) # turn on motor during 5 sec.
+                time.sleep(120) # and wait 2 min.
+                
+            #elif tds_Value > tds_Max
+            elif tds_Value > 200: # when tds value is lower than maximum tds.
+                motor_8run(1) # turn on motor during 30 sec.
+                time.sleep(120) # and wait 2 min.
+                
+            else:
+                time.sleep(1)
+        
+print("main thread start")
+#LED THREAD
+led = LED()
+led.start()
 
+#WATER_LEVEL THREAD
+level = LEVEL()
+level.start()
 
-def consumer(q):
-    while True:
-        data, evt = q.get()
-        print('Receive Original Data: {}'.format(data))
-        processed = data * 5
-        print('Receive Processed Data: {}'.format((processed)))
-        print('\n')
-        evt.set()
-        q.task_done()
+#MOTOR THREAD
+motor = MOTOR()
+motor.start()
+
+#BLUETOOTH THREAD
+server_socket= BluetoothSocket(RFCOMM)
+server_socket,client_socket,address = bt2.bt_setup(server_socket)
+while True:
+    data = bt2.read(client_socket)
+    print("Received: %s" %data)
+    
+    if('A' in data):
+        if(data[0]=='2'):
+            print("Auto Mode ON")
+        elif(data[0]=='1'):
+            print("Manual Mode ON")
+        else:
+            print("data error")
+            
+    elif('M' in data):
+        if(data[0]=='1'):
+            print("LED ON")
+        elif(data[0]=='2'):
+            print("LED OFF")
+        else:
+            print("data error")
+            
+    elif('#' in data):
+        if(data[0]=='1'):
+            print("TDS: lettuce mode")
+        elif(data[0]=='2'):
+            print("TDS: chives mode")
+        elif(data[0]=='3'):
+            print("TDS: strawberry mode")
+        else:
+            printf("data error")
+            
+    else:
+        print("Quit")
+        break;
+
+# JOIN
+led.join()
+level.join()
+motor.join()
+bt2.close()
+print("main thread end")
