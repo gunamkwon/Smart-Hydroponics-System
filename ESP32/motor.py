85bb7d781d7d08b0dd05bf7712e1d4082c2793fa
gunamkwon <rnska06@mju.ac.kr>
2021-05-12 12:55:16 +0900

    Pi4 motor

--- ESP32/motor.py
+++ ESP32/motor.py
@@ -1,14 +1,30 @@
 import time
 import RPi.GPIO as GPIO
 
-
 def motor_setup():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(4, GPIO.OUT)  # GPIO 4 motor OUTPUT
+    GPIO.setup(7, GPIO.OUT) ##GPIO 7 motor OUTPUT
+    GPIO.setup(8, GPIO.OUT) ##GPIO 8 motor OUTPUT
 
+def motor_7run(on_time):
+    GPIO.output(7, False)
+    print('motor 7 ON!!!!!')
+    time.sleep(on_time)
+    GPIO.output(7, True)
+    print('motor 7 OFF')
 
-def motor_run(second, second2):
-    GPIO.output(4, False)
-    time.sleep(second)
-    GPIO.output(4, True)
-    time.sleep(second2)
+def motor_8run(on_time):
+    GPIO.output(8, False)
+    print('motor 8 ON!!!!!')
+    time.sleep(on_time)
+    GPIO.output(8, True)
+    print('motor 8 OFF')
+    
+if __name__ == '__main__':
+    GPIO.cleanup()
+    motor_setup()
+    motor_7run(3)
+    time.sleep(1)
+    motor_8run(3)
+    time.sleep(1)
+    
