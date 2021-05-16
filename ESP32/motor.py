25dfee869f5c2c2e40587ca8c095107c60108a84
Doan-Kang <ehdksrkd@gmail.com>
2021-05-16 14:45:22 +0900

    motor6 추가 및 재배종에 따라 tds범위 조

--- ESP32/motor.py
+++ ESP32/motor.py
@@ -3,8 +3,16 @@ import RPi.GPIO as GPIO
 
 def motor_setup():
     GPIO.setmode(GPIO.BCM)
+    GPIO.setup(6, GPIO.OUT) ##GPIO 6 motor OUTPUT
     GPIO.setup(7, GPIO.OUT) ##GPIO 7 motor OUTPUT
     GPIO.setup(8, GPIO.OUT) ##GPIO 8 motor OUTPUT
+
+def motor_6run(on_time):
+    GPIO.output(6, False)
+    print('motor 6 ON!!!!!')
+    time.sleep(on_time)
+    GPIO.output(6, True)
+    print('motor 6 OFF')
 
 def motor_7run(on_time):
     GPIO.output(7, False)
@@ -23,8 +31,10 @@ def motor_8run(on_time):
 if __name__ == '__main__':
     GPIO.cleanup()
     motor_setup()
-    motor_7run(3)
+    motor_6run(1)
     time.sleep(1)
-    motor_8run(3)
+    motor_7run(1)
+    time.sleep(1)
+    motor_8run(1)
     time.sleep(1)
     
